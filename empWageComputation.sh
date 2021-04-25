45b71307db14e1500e97cd66b9b6bc8cf9ab55d3
karuna <karunakakade8@gmail.com>
2021-04-25 22:52:26 +0530

    Added: calculate employee wage using case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,19 +2,19 @@
 
 emp=$((RANDOM%2))
 
-if [ $emp -eq 1 ]
-then
- ratePerHr=20
- empHrs=8
- salary=$(($ratePerHr*$empHrs))
-elif [ $emp -eq 0 ]
-then
- ratePerHr=20
- empHrs=4
- salary=$(($ratePerHr*$empHrs))
-else
- salary=0
-fi
-
-echo Salary is: $salary
-
+case $emp in
+   0)
+	 ratePerHr=20
+ 	 empHrs=8
+ 	 salary=$(($ratePerHr*$empHrs))
+    echo $salary
+ 	;;
+	1)
+ 	ratePerHr=20
+ 	empHrs=4
+ 	salary=$(($ratePerHr*$empHrs))
+	echo $salary
+	;;
+	*)
+		echo invalid input
+esac
