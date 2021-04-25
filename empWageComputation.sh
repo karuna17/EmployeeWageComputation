eff16d35a54118f67d07cada189b71b4cb692976
karuna <karunakakade8@gmail.com>
2021-04-25 21:18:39 +0530

    Added: Add part time employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
- 
-empCheck=$((RANDOM%2))
-if [ $empCheck -eq 1 ]
-then 
- echo Employee is present
+
+emp=$((RANDOM%2))
+
+if [ $emp -eq 1 ]
+then
+ ratePerHr=20
+ empHrs=8
+ salary=$(($ratePerHr*$empHrs))
+elif [ $emp -eq 0 ]
+then
+ ratePerHr=20
+ empHrs=4
+ salary=$(($ratePerHr*$empHrs))
 else
- echo Employee is Absent
+ salary=0
 fi
+
+echo Salary is: $salary
+
